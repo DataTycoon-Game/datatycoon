3779c3eeed622a5418ee254d744e15b1a80a538a
Georgi Aleksandrov (Krezme) <karakonjo5818@gmail.com>
2018-12-06 00:40:26 +0200

    Increment Counter Finished(can be improved)
    When the player touches the phone the "Money" increases by one. It can be improved.

--- Assets/Scripts/IncrementScore.cs
+++ Assets/Scripts/IncrementScore.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class IncrementScore : MonoBehaviour {
 
-	// Use this for initialization
+	public Text textMoney;
+	private int money;
+
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.touchCount == 1) {
+			if (Input.GetTouch(0).phase == TouchPhase.Began) {
+				money++;
+				textMoney.text = "Money: " + money.ToString();
+			}
+		}
 	}
 }
