7a70d7c2e96c045390becaf78afc4bbd7511efb0
Georgi Aleksandrov (Krezme) <karakonjo5818@gmail.com>
2018-12-08 01:03:31 +0200

    Changing image with a button
    The button allows you to buy now rooms and it decreses the cost from your money. Dosen't need any additional written code when adding a new room image in the textures array.

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+
+	public Text textMoney;
+	public int money;
+	private ImageManager imageManager;
+	public Text buttonUpgrade;
+
+	void Start() {
+		imageManager = FindObjectOfType<ImageManager>();
+	} 
+
+	void Update()
+	{
+		Touches();
+		ButtonText();
+	}
+
+	void Touches()
+	{
+		if (Input.touchCount > 0)
+		{
+			if (Input.GetTouch(0).phase == TouchPhase.Began)
+			{
+				money++;
+				textMoney.text = "Money: " + money.ToString();
+			}
+		}
+	}
+
+	void ButtonText() {
+		if (imageManager.textures.Length >= imageManager.i + 1)
+		{
+			buttonUpgrade.text = "Buy next upgrade for: " + imageManager.upgradeCost.ToString();
+		}
+		else {
+			buttonUpgrade.text = "Upgrade Maxed Out";
+		}
+	}
+}
