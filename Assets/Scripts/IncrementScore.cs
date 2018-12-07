6d9c9a4ed1f3df0cb5ce608719d23753751081ee
Georgi Aleksandrov (Krezme) <karakonjo5818@gmail.com>
2018-12-07 15:44:41 +0200

    Better version of the IncrementScore script v1.1
    Working

--- Assets/Scripts/IncrementScore.cs
+++ Assets/Scripts/IncrementScore.cs
@@ -3,20 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class IncrementScore : MonoBehaviour {
+public class IncrementScore : MonoBehaviour
+{
 
 	public Text textMoney;
 	private int money;
-	private Touch touches;
 
-	void Start () {
-		
+	void Update()
+	{
+		Touches();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.touchCount == 1) {
-			if (Input.GetTouch(0).phase == TouchPhase.Began) {
+
+	void Touches()
+	{
+		if (Input.touchCount > 0)
+		{
+			if (Input.GetTouch(0).phase == TouchPhase.Began)
+			{
 				money++;
 				textMoney.text = "Money: " + money.ToString();
 			}
