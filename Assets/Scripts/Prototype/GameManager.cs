5d944019fa8d3bc4b62af6b8ee46ff0544b7dd81
Georgi Aleksandrov (Krezme) <karakonjo5818@gmail.com>
2019-03-02 22:20:44 +0200

    Uploading prototype
    Scene prototype and GameManager script

---
+++ Assets/Scripts/Prototype/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public Text text;
+	private int money;
+
+	void Start(){
+		money = 0;
+	}
+
+	public void Increment(){
+		money++;
+		text.text = ("Money= " + money);	}
+}
